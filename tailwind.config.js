9ff2c1aeafb2f742443adede056c2301f4360787
ahmed saed <103289562+ahmedsaed27@users.noreply.github.com>
2024-05-14 11:52:54 +0300

    ProductionUpdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./resources/**/*.blade.php",
-//     "./resources/**/*.js",
-//     "./resources/**/*.vue",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-import preset from './vendor/filament/support/tailwind.config.preset'
-
+/** @type {import('tailwindcss').Config} */
 export default {
-    presets: [preset],
-    content: [
-        './resources/views/**/*.blade.php',
-        './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
-    ],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    './resources/views/filament/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
+// import preset from './vendor/filament/support/tailwind.config.preset'
+
+// export default {
+//     presets: [preset],
+//     content: [
+//         './resources/views/**/*.blade.php',
+//         './app/Filament/**/*.php',
+//         './resources/views/filament/**/*.blade.php',
+//         './vendor/filament/**/*.blade.php',
+//     ],
+// }
 
